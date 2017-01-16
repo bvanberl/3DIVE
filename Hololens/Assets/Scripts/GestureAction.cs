070b3abaa6907e26a4c138716bf75d2260b5510d
Blake VanBerlo <bvanberl@uwo.ca>
2017-01-16 14:27:53 -0500

    first commit yo

---
+++ Hololens/Assets/Scripts/GestureAction.cs
@@ -0,0 +1,67 @@
+﻿using Academy.HoloToolkit.Unity;
+using UnityEngine;
+
+/// <summary>
+/// GestureAction performs custom actions based on 
+/// which gesture is being performed.
+/// </summary>
+public class GestureAction : MonoBehaviour
+{
+    [Tooltip("Rotation max speed controls amount of rotation.")]
+    public float RotationSensitivity = 10.0f;
+
+    private Vector3 manipulationPreviousPosition;
+    
+    private float xRotationFactor, yRotationFactor, zRotationFactor;
+
+    void Update()
+    {
+        PerformRotation();
+    }
+
+    private void PerformRotation()
+    {
+        if (GestureManager.Instance.IsNavigating &&
+            (!ExpandModel.Instance.IsModelExpanded ||
+            (ExpandModel.Instance.IsModelExpanded && HandsManager.Instance.FocusedGameObject == gameObject)))
+        {
+            /* TODO: DEVELOPER CODING EXERCISE 2.c */
+            /*
+            // 2.c: Calculate rotationFactor based on GestureManager's NavigationPosition.X and multiply by RotationSensitivity.
+            // This will help control the amount of rotation.
+            rotationFactor = GestureManager.Instance.NavigationPosition.x * RotationSensitivity;
+
+            // 2.c: transform.Rotate along the Y axis using rotationFactor.
+            transform.Rotate(new Vector3(0, -1 * rotationFactor, 0));
+               */
+            xRotationFactor = GestureManager.Instance.NavigationPosition.x * RotationSensitivity;
+            yRotationFactor = GestureManager.Instance.NavigationPosition.y * RotationSensitivity;
+            zRotationFactor = GestureManager.Instance.NavigationPosition.z * RotationSensitivity;
+            Debug.Log(xRotationFactor + " " + yRotationFactor + " " + zRotationFactor);
+            // 2.c: transform.Rotate along the Y axis using rotationFactor.
+            transform.Rotate(new Vector3(-1 * xRotationFactor, -1 * yRotationFactor, -1 * zRotationFactor));
+        }
+    }
+
+    void PerformManipulationStart(Vector3 position)
+    {
+        manipulationPreviousPosition = position;
+    }
+
+    void PerformManipulationUpdate(Vector3 position)
+    {
+        if (GestureManager.Instance.IsManipulating)
+        {
+            /* TODO: DEVELOPER CODING EXERCISE 4.a */
+
+            Vector3 moveVector = Vector3.zero;
+            // 4.a: Calculate the moveVector as position - manipulationPreviousPosition.
+            moveVector = position - manipulationPreviousPosition;
+            // 4.a: Update the manipulationPreviousPosition with the current position.
+            manipulationPreviousPosition = position;
+
+            // 4.a: Increment this transform's position by the moveVector.
+            transform.position += moveVector;
+        }
+    }
+}
---
+++ Hololens/Assets/Scripts/Loader.cs
@@ -0,0 +1,120 @@
+﻿// loads the raw binary data into a texture saved as a Unity asset 
+// (so can be de-activated after a given data cube has been converted)
+// adapted from a XNA project by Kyle Hayward 
+// http://graphicsrunner.blogspot.ca/2009/01/volume-rendering-101.html
+// Gilles Ferrand, University of Manitoba 2016
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+using UnityEngine;
+using System.IO; // to get BinaryReader
+using System.Linq; // to get array's Min/Max
+using System.Collections.Generic;
+using System;
+
+#if !UNITY_EDITOR
+using Windows.Networking;
+using Windows.Networking.Sockets;
+#endif
+
+
+public class Loader : MonoBehaviour {
+
+    public int[] size;
+	public bool mipmap;
+    public int noSlices = 0;
+    
+    void Start() {
+        #if !UNITY_EDITOR
+        //socketShit();
+        #endif
+
+        size = new int[3] { 256, 256, 256 };
+        // load the raw data
+        Color[] colors = LoadRAWFile();
+		// create the texture
+		Texture3D texture = new Texture3D (size[0], size[1], size[2], TextureFormat.Alpha8, mipmap);
+		texture.SetPixels (colors);
+		texture.Apply ();
+		// assign it to the material of the parent object
+		GetComponent<Renderer>().material.SetTexture ("_Data", texture);
+		// save it as an asset for re-use
+
+	}
+
+	private Color[] LoadRAWFile()
+	{/*
+        Color[] colors;
+
+		Debug.Log ("Opening file "+path+filename+extension);
+		FileStream file = new FileStream(path+filename+extension, FileMode.Open);
+		Debug.Log ("File length = "+file.Length+" bytes, Data size = "+size[0]*size[1]*size[2]+" points -> "+file.Length/(size[0]*size[1]*size[2])+" byte(s) per point");
+
+		BinaryReader reader = new BinaryReader(file);
+		byte[] buffer = new byte[size[0] * size[1] * size[2]]; // assumes 8-bit data
+		reader.Read(buffer, 0, sizeof(byte) * buffer.Length);
+        reader.BaseStream.Dispose();
+
+		colors = new Color[buffer.Length];
+		Color color = Color.black;
+		for (int i = 0; i < buffer.Length; i++)
+		{
+			color.a = (float)buffer[i] / byte.MaxValue; //scale the scalar values to [0, 1]
+			colors [i] = color;
+		}
+
+		return colors;*/
+        
+        TextAsset csv = Resources.Load("pixelData") as TextAsset;
+        string[] values = csv.text.Split(',');
+        size[0] = Convert.ToInt32(values[0]);
+        size[1] = Convert.ToInt32(values[1]);
+        size[2] = Convert.ToInt32(values[2]);
+        int max = Convert.ToInt32(values[3]);
+        Color[] colours = new Color[values.Length - 4];
+        int index = 0;
+        for (int i = 4; i < values.Length - 1; i++)
+        {
+            colours[index] = Color.black;
+            colours[index++].a = (float)(Convert.ToInt32(values[i])) / max;
+        }
+
+        return colours;
+    }
+
+#if !UNITY_EDITOR
+    async public void socketShit()
+    {
+
+         HostName hostName;
+        StreamSocket socket = null;
+            using (socket = new StreamSocket())
+            {
+                hostName = new HostName("127.0.0.1");
+
+                // Set NoDelay to false so that the Nagle algorithm is not disabled
+                socket.Control.NoDelay = false;
+
+                try
+                {
+                    // Connect to the server
+                    await socket.ConnectAsync(hostName, "2500");
+                }
+                catch (Exception exception)
+                {
+                    switch (SocketError.GetStatus(exception.HResult))
+                    {
+                        case SocketErrorStatus.HostNotFound:
+                            // Handle HostNotFound Error
+                            throw;
+                        default:
+                            // If this is an unknown status it means that the error is fatal and retry will likely fail.
+                            throw;
+                    }
+                }
+            }
+
+    }
+#endif
+}
